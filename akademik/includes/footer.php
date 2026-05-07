6992cbf206b96e29eb6771f2be0c5ed706f7cf2d
Nabil <nabilnazrilfikar@gmail.com>
2026-05-07 15:18:01 +0700

    Delete akademik directory

--- akademik/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    </main><!-- end main-content -->
-</div><!-- end main-wrapper -->
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?= APP_URL ?>/js/main.js"></script>
-</body>
-</html>
--- akademik/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-// logout.php
-require_once 'includes/config.php';
-require_once 'includes/auth.php';
-logout();
